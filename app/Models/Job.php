29a2dee52915aeaca967558041a39506008c22a4
Kamal Lachinov <kamallachinov5@gmail.com>
2025-01-30 17:30:42 +0400

    batman

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,6 +10,18 @@ class Job extends Model
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
+    public function tag(string $name)
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+        $this->tags()->attach($tag);
+    }
+
+    //a job can belong to many and have many tags
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
     //Job belongs to an employer
     public function employer()
     {
