540706ebac47fb1ed6b9bc9ff128101bde99e192
Kamal Lachinov <kamallachinov5@gmail.com>
2025-01-31 17:25:41 +0400

    wrote jobcontroller store method

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,6 +6,10 @@ use App\Models\Job;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class JobController extends Controller
 {
@@ -28,16 +32,38 @@ class JobController extends Controller
      */
     public function create()
     {
-        //
+        return view('jobs.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreJobRequest $request)
+    public function store(Request $request)
     {
-        //
+        $attributes = $request->validate([
+            'title' => ['required'],
+            'salary' => ['required'],
+            'location' => ['required'],
+            'schedule' => ['required', Rule::in(['Full Time', 'Part Time'])],
+            'url' => ['required', 'active_url', 'url'],
+            'tags' => ['nullable']
+        ]);
+
+        $attributes['featured'] = $request->has('featured');
+
+        $job =  Auth::user()->employer->jobs()->create(Arr::except($attributes, 'tags'))->tags();
+        if ($attributes['tags']) {
+            // laravel,backend => ['laravel', 'backend']
+            foreach (explode(",", $attributes['tags']) as $tag) {
+                $job->tag($tag);
+            }
+        }
+
+        return redirect('/');
     }
+
+
+
 
     /**
      * Display the specified resource.
---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,27 @@
+<x-layout>
+    <x-page-heading>
+        Create a Job
+    </x-page-heading>
+
+    <x-forms.form action="/jobs" method="POST">
+        <x-forms.input name="title" label="Title" placeholder="CEO" />
+        <x-forms.input name="salary" label="Salary" placeholder="$90,000 USD" />
+        <x-forms.input name="location" label="Location" placeholder="Winter Park, Florida" />
+        <x-forms.select name="schedule" label="Schedule">
+            <option>Part time</option>
+            <option>Full time</option>
+        </x-forms.select>
+        <x-forms.input name="url" label="URL" placeholder="https://acme.com/jobs/ceo-wanted" />
+        <x-forms.checkbox label="Feature (Costs Extra)" name="featured" />
+
+        <x-forms.divider />
+
+        <x-forms.input name="tags" label="Tags (comma seperated)" placeholder="coding, video, education" />
+
+
+        <x-forms.button>Publish</x-forms.button>
+    </x-forms.form>
+
+
+
+</x-layout>
