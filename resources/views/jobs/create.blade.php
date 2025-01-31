5cd5ec2fa148a7db65be89ef9d7faadc2b4c4768
Kamal Lachinov <kamallachinov5@gmail.com>
2025-01-31 17:27:31 +0400

    feat: add csrf token and method inside form

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -4,6 +4,8 @@
     </x-page-heading>
 
     <x-forms.form action="/jobs" method="POST">
+        @csrf
+        @method('POST')
         <x-forms.input name="title" label="Title" placeholder="CEO" />
         <x-forms.input name="salary" label="Salary" placeholder="$90,000 USD" />
         <x-forms.input name="location" label="Location" placeholder="Winter Park, Florida" />
